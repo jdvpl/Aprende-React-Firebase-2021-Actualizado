7c058327ea8cfcd7a3a792bccffcced8074fe704
Cristhian Caycedo <cacaycedo@gov.co>
2021-11-14 18:53:14 -0500

    imprimiendo data

--- 2crudfake/src/components/Usuario.js
+++ 2crudfake/src/components/Usuario.js
@@ -1,19 +1,25 @@
 import React,{useState, useEffect} from 'react';
 import axios from 'axios';
-import {Link} from 'react-router-dom'
+import {Link,useParams} from 'react-router-dom'
 const Usuario = () => {
     const [user, setuser] = useState([]);
+    const {id}=useParams();
     const obtenerUsuarios=async()=>{
-        const res=await axios.get(`https://jsonplaceholder.typicode.com/users/`);
+        const res=await axios.get(`https://jsonplaceholder.typicode.com/users/${id}`);
         const data=await res.data;
         setuser(data);
     }
     useEffect(() => {
         obtenerUsuarios();
     }, [])
+    console.log(user)
+    const {name, username,email, address}=user;
     return (
         <div>
-            <h4>Usuario</h4>
+            <h4>Usuario {id}</h4>
+            <p>{name}</p>
+            <p>{username}</p>
+            <p>{email}</p>
         </div>
     )
 }
